d8fbe963208f46fdfc3fbf20c93c9f62a4255d6f
BYVoid <byvoid@byvoid.com>
2012-09-21 04:56:02 +0800

    Moved sources into lib.

--- continuation.js
+++ continuation.js
@@ -2,8 +2,8 @@ var util = require('util');
 var esmangle = require('esmangle');
 var escodegen = require('escodegen');
 
-var parser = require('./parser');
-var transform = require('./transform');
+var parser = require('./lib/parser');
+var transform = require('./lib/transform');
 
 exports.transform = function (code) {
   var ast = parser.parse(code);
